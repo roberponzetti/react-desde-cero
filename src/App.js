d5d20774406794c88185924b0a42d54ea3007e86
PONZETTO <roberponzetti@gmail.com>
2021-03-22 10:29:55 -0300

    codigo desp del array

--- src/App.js
+++ src/App.js
@@ -3,18 +3,33 @@ import Curso from './Curso';
 import logo from './logo.svg';
 import "./styles/styles.scss";
 
-//const App = () => <h1>Hola Mundo desde -> App.js</h1> //App se podria llamar de otra manera pero por buena practica lo ponemos asi
+const cursos = [
+  {
+    "title": "React desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/small/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+    "price": 30,
+    "profesor": "Robertino Ponzetti"
+  },
+  {
+    "title": "HTML desde cero",
+    "image": "algo.jpg",
+    "price": 40,
+    "profesor": "Gabriel Martin"
+  },
+  {
+    "title": "C# desde cero",
+    "image": "algo.jpg",
+    "price": 50,
+    "profesor": "Otro Profesor"
+  },
+  {
+    "title": "Java desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/small/61e5a210-8dab-412e-a6dc-802c070cc18c.jpg",
+    "price": 60,
+    "profesor": "Jenifer Paira"
+  },
+]
 const App = () => (
-  // <Fragment>
-  //   <div className="saludo">
-  //     <h1>Hola mundo</h1>
-  //     <p>Hola saludando desde el parrafo</p>
-  //   </div>
-  //   <div>
-  //     <h2>Hola mundo2</h2>
-  //     <p>Hola saludando desde el parrafo2</p>
-  // </div>
-  // </Fragment>
   <Fragment>
     <div className="main-banner img-container l-section" id="main-banner">
       <div className="ed-grid lg-grid-6">
@@ -29,12 +44,9 @@ const App = () => (
       </div>
     </div>
     <div className="ed-grid m-grid-3">
-      <Curso/>
-      <Curso/>
-      <Curso/>
-      <Curso/>
-      <Curso/>
-      <Curso/>
+      {
+        cursos.map(c => <Curso  title={c.title} image={c.image} price={c.price} profesor={c.profesor} />)
+      }
     </div>
   </Fragment>
 );
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Curso = () => (
+const Curso = ({title,image,price,profesor}) => (
     <article className="card">
         <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-          <img src="https://ux.ed.team/img/curso-portada.png" alt="Poster"/>
+          <img src={image} alt={title}/>
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-          <h3 className="t5 s-mb-2 s-center">
-            Programación orientada a objetos con Go
-          </h3>
-          <div className="s-mb-2 s-main-center">
-            <div className="card__teacher s-cross-center">
-              <div className="card__avatar s-mr-1">
-                <div className="circle img-container">
-                  <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/eb52cf60-d29a-4b6d-a8cf-75c902b87fa7.png" alt="Profesor"/>
-                </div>
-              </div>
-              <span className="small">Alexys Lozada</span>
-            </div>
+          <h3 className="center">{title}</h3>
+          <div className="s-main-center">
+            {profesor}
           </div>
           <div className="s-main-center">
-            <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+            <a className="button--ghost-alert button--tiny" href="#">{`$ ${price}`}</a>
           </div>
         </div>
       </article>
-);
+)
+Curso.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.string,
+  profesor: PropTypes.string
+}
+
+Curso.defaultProps = {
+  title: "No se encontro titulo",
+  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png",
+  price: "-",
+  profesor: "Unknown"
+}
 
 export default Curso;
